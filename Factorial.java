366d34558f5f6aafdc13267d2a671a90ee7823da
Akshay <akshaypetkar7776@gmail.com>
2022-09-22 20:13:57 +0530

    first commit

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+package Assignments;
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number to find factorial");
+		int num = sc.nextInt();
+		int mult=1;
+		for(int i=1; i<=num; i++)
+		{
+			mult=mult*i;
+		}
+		System.out.println("Factorial is "+mult);
+	}
+
+}
+
+/* OUTPUT
+Enter number to find factorial
+12
+Factorial is 479001600
+*/
